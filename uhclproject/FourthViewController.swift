e6fc34c51a1088706b4ca78dfeed9fc8c2a2bcc1
Abhishek Kumar <ak2588099@gmail.com>
2016-02-22 16:18:27 -0600

    Initial layout design

--- uhclproject/FourthViewController.swift
+++ uhclproject/FourthViewController.swift
@@ -1,26 +1,25 @@
 //
-//  FirstViewController.swift
+//  FourthViewController.swift
 //  uhclproject
 //
 //  Created by Abhishek Kumar on 2/10/16.
 //  Copyright © 2016 Abhishek Kumar. All rights reserved.
 //
 
+
 import UIKit
 
-class FirstViewController: UIViewController {
-
-   
+class FourthViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
--- uhclproject/ThirdViewController.swift
+++ uhclproject/ThirdViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  ThirdViewController.swift
 //  uhclproject
 //
 //  Created by Abhishek Kumar on 2/10/16.
@@ -8,19 +8,17 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
-   
+class ThirdViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
